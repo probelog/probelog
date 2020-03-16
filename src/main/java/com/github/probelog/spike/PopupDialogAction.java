3b89ea30976f11e1ea53231726a9201e473bae55
dave.halpin <dave.halpin@synchronoss.com>
2020-03-16 15:52:10 +0000

    Initial Plugin Spike - listens for file saves

---
+++ src/main/java/com/github/probelog/spike/PopupDialogAction.java
@@ -0,0 +1,39 @@
+package com.github.probelog.spike;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.vfs.VirtualFileManager;
+import com.intellij.openapi.vfs.newvfs.BulkFileListener;
+import com.intellij.openapi.vfs.newvfs.events.VFileEvent;
+import com.intellij.util.messages.MessageBusConnection;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class PopupDialogAction extends AnAction {
+
+    @Override
+    public void update(AnActionEvent e) {
+        // Using the event, evaluate the context, and enable or disable the action.
+    }
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+
+        // this works ;-)
+
+        MessageBusConnection connection = ApplicationManager.getApplication().getMessageBus().connect();
+
+        connection.subscribe(VirtualFileManager.VFS_CHANGES, new BulkFileListener() {
+            @Override
+            public void after(@NotNull List<? extends VFileEvent> events) {
+                for (VFileEvent event: events) {
+                    if (event.isFromSave())
+                        System.out.println("event = " + event);
+                }
+            }
+        });
+    }
+
+}
