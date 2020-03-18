73c8a5769293d2e80545eeda939d64f309b7e632
dave.halpin <dave.halpin@synchronoss.com>
2020-03-18 12:34:19 +0000

    Initial Plugin Spike - File Event Spike Results

--- src/main/java/com/github/probelog/spike/PopupDialogAction.java
+++ src/main/java/com/github/probelog/spike/PopupDialogAction.java
@@ -3,6 +3,7 @@ package com.github.probelog.spike;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.VirtualFileManager;
 import com.intellij.openapi.vfs.newvfs.BulkFileListener;
 import com.intellij.openapi.vfs.newvfs.events.VFileEvent;
@@ -23,11 +24,44 @@ public class PopupDialogAction extends AnAction {
     Spikes
 
     - Create
+
+    VFileCreateEvent
+
+    Spike event = VfsEvent[update: file:///Users/dave.halpin/git/bt-pc/usermigrator/src/test/test1.txt]
+    Spike canonicalPath = /Users/dave.halpin/git/bt-pc/usermigrator/src/test/test1.txt
+    Spike name          = test1.txt
+    Spike path          = /Users/dave.halpin/git/bt-pc/usermigrator/src/test/test1.txt
+    Spike parent name   = test
+
+
     - Update
-    - refactor (rename of field type)
-    - Rename
+
+    VFileContentChangeEvent
+
+    Spike event = VfsEvent[update: file:///Users/dave.halpin/git/bt-pc/usermigrator/src/test/test1.txt]
+    Spike canonicalPath = /Users/dave.halpin/git/bt-pc/usermigrator/src/test/test1.txt
+    Spike name          = test1.txt
+    Spike path          = /Users/dave.halpin/git/bt-pc/usermigrator/src/test/test1.txt
+    Spike parent name   = test
+
+    - refactor (rename)
+
+    Spike event = VfsEvent[property(name) changed for 'file:///Users/dave.halpin/git/bt-pc/usermigrator/src/test/test4.txt': test3.txt -> test4.txt]
+    Spike canonicalPath = /Users/dave.halpin/git/bt-pc/usermigrator/src/test/test4.txt
+    Spike name          = test4.txt
+    Spike path          = /Users/dave.halpin/git/bt-pc/usermigrator/src/test/test4.txt
+    Spike parent name   = test
+
+    VFilePropertyChangeEvent
+    VfsEvent[property(name) changed for 'file:///Users/dave.halpin/git/bt-pc/usermigrator/src/test/test5.txt': test4.txt -> test5.txt]
+
+    - Move
+    VFileMoveEvent
+
+
     - Delete
-       - Delete and create with same name
+    VFileDeleteEvent
+
     - Test Run Listener
 
      */
@@ -43,11 +77,14 @@ public class PopupDialogAction extends AnAction {
             @Override
             public void after(@NotNull List<? extends VFileEvent> events) {
                 for (VFileEvent event: events) {
-                    if (event.isFromSave()) {
-                        System.out.println("event = " + event);
-                        System.out.println("event = " + event);
-                        event.getFile();
-                    }
+                    //if (event.isFromSave()) {
+                        System.out.println("Spike event = " + event);
+                        VirtualFile file = event.getFile();
+                        System.out.println("Spike canonicalPath = " + file.getCanonicalPath());
+                        System.out.println("Spike name          = " + file.getName());
+                        System.out.println("Spike path          = " + file.getPath());
+                        System.out.println("Spike parent name   = " + file.getParent().getName());
+                    //}
                 }
             }
         });
