b37ad1cd9b44841aec2e3bb64e6417af1f9cc5d2
dave.halpin <dave.halpin@synchronoss.com>
2020-03-20 12:55:41 +0000

    Initial Plugin Spike - Test Run Listener Investigation

--- src/main/java/com/github/probelog/spike/PopupDialogAction.java
+++ src/main/java/com/github/probelog/spike/PopupDialogAction.java
@@ -1,14 +1,18 @@
 package com.github.probelog.spike;
 
+import com.intellij.execution.testframework.sm.runner.SMTRunnerEventsAdapter;
+import com.intellij.execution.testframework.sm.runner.SMTRunnerEventsListener;
+import com.intellij.execution.testframework.sm.runner.SMTestProxy;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.openapi.vfs.VirtualFileManager;
+import com.intellij.openapi.vfs.*;
 import com.intellij.openapi.vfs.newvfs.BulkFileListener;
 import com.intellij.openapi.vfs.newvfs.events.VFileEvent;
+import com.intellij.rt.execution.junit.IDEAJUnitListener;
 import com.intellij.util.messages.MessageBusConnection;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
@@ -55,14 +59,39 @@ public class PopupDialogAction extends AnAction {
     VFilePropertyChangeEvent
     VfsEvent[property(name) changed for 'file:///Users/dave.halpin/git/bt-pc/usermigrator/src/test/test5.txt': test4.txt -> test5.txt]
 
+  public Object getNewValue()
+  public Object getOldValue()
+  case VirtualFile.PROP_NAME: ----> public static final String PROP_NAME = "name";
+  public String getPropertyName()
+
     - Move
-    VFileMoveEvent
+    public class VirtualFileMoveEvent extends VirtualFileEvent {
+    public VirtualFile getOldParent()
+    public VirtualFile getNewParent()
 
 
     - Delete
     VFileDeleteEvent
 
-    - Test Run Listener
+    also Copy
+
+    public class VFileCopyEvent
+  public VirtualFile getNewParent()
+
+
+  Test Run Listener
+
+ junit 5 extensions - https://junit.org/junit5/docs/5.4.0-RC2/user-guide/index.html
+
+https://junit.org/junit5/docs/5.4.0-RC2/user-guide/index.html#extensions-registration-automatic
+ Package org.junit.jupiter.api.extension
+Interface TestWatcher
+
+in plugin.xml there is this
+
+    <extensions defaultExtensionNs="com.intellij">
+        <!-- Add your extensions here -->
+    </extensions>
 
      */
 
@@ -76,18 +105,118 @@ public class PopupDialogAction extends AnAction {
         connection.subscribe(VirtualFileManager.VFS_CHANGES, new BulkFileListener() {
             @Override
             public void after(@NotNull List<? extends VFileEvent> events) {
-                for (VFileEvent event: events) {
-                    //if (event.isFromSave()) {
-                        System.out.println("Spike event = " + event);
-                        VirtualFile file = event.getFile();
-                        System.out.println("Spike canonicalPath = " + file.getCanonicalPath());
-                        System.out.println("Spike name          = " + file.getName());
-                        System.out.println("Spike path          = " + file.getPath());
-                        System.out.println("Spike parent name   = " + file.getParent().getName());
-                    //}
+                for (VFileEvent event : events) {
+                    System.out.println("Spike event = " + event);
+                    VirtualFile file = event.getFile();
+                    System.out.println("Spike canonicalPath = " + file.getCanonicalPath());
+                    System.out.println("Spike name          = " + file.getName());
+                    System.out.println("Spike path          = " + file.getPath());
+                    System.out.println("Spike parent name   = " + file.getParent().getName());
                 }
             }
         });
+
+        final IDEAJUnitListener junitListener = new IDEAJUnitListener() {
+            @Override
+            public void testStarted(String s, String s1) {
+
+            }
+
+            @Override
+            public void testFinished(String s, String s1) {
+                System.out.println("Spike Test run - arg 1: " + s);
+                System.out.println("Spike Test run - arg 2: " + s1);
+            }
+        };
+
+        final SMTRunnerEventsListener smtRunnerEventsListener = new SMTRunnerEventsListener() {
+            @Override
+            public void onTestingStarted(@NotNull SMTestProxy.SMRootTestProxy smRootTestProxy) {
+
+            }
+
+            @Override
+            public void onTestingFinished(@NotNull SMTestProxy.SMRootTestProxy smRootTestProxy) {
+                System.out.println("Spike testproxy:" + smRootTestProxy);
+                for (SMTestProxy test: smRootTestProxy.getAllTests()) {
+                    System.out.println("Spike Test: " + test.getName() + ", Pass: " + test.isPassed());
+                }
+            }
+
+            @Override
+            public void onTestsCountInSuite(int i) {
+
+            }
+
+            @Override
+            public void onTestStarted(@NotNull SMTestProxy smTestProxy) {
+
+            }
+
+            @Override
+            public void onTestFinished(@NotNull SMTestProxy smTestProxy) {
+
+            }
+
+            @Override
+            public void onTestFailed(@NotNull SMTestProxy smTestProxy) {
+
+            }
+
+            @Override
+            public void onTestIgnored(@NotNull SMTestProxy smTestProxy) {
+
+            }
+
+            @Override
+            public void onSuiteFinished(@NotNull SMTestProxy smTestProxy) {
+
+            }
+
+            @Override
+            public void onSuiteStarted(@NotNull SMTestProxy smTestProxy) {
+
+            }
+
+            @Override
+            public void onCustomProgressTestsCategory(@Nullable String s, int i) {
+
+            }
+
+            @Override
+            public void onCustomProgressTestStarted() {
+
+            }
+
+            @Override
+            public void onCustomProgressTestFailed() {
+
+            }
+
+            @Override
+            public void onCustomProgressTestFinished() {
+
+            }
+
+            @Override
+            public void onSuiteTreeNodeAdded(SMTestProxy smTestProxy) {
+
+            }
+
+            @Override
+            public void onSuiteTreeStarted(SMTestProxy smTestProxy) {
+
+            }
+        };
+
+        connection.subscribe(SMTRunnerEventsListener.TEST_STATUS, new SMTRunnerEventsAdapter() {
+                    @Override
+                    public void onTestFinished(@NotNull SMTestProxy test) {
+                        System.out.println("Spike Test: " + test.getName() + ", Pass: " + test.isPassed());
+                    }
+                }
+            );
+
     }
 
 }
