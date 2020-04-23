aea84bb86aa9472be5d1adcc5f75f4e6772e4506
dave.halpin <dave.halpin@synchronoss.com>
2020-04-23 15:27:05 +0100

    Two Files in a Panel

--- src/main/java/com/github/probelog/PanelBuilder.java
+++ src/main/java/com/github/probelog/PanelBuilder.java
@@ -2,19 +2,41 @@ package com.github.probelog;
 
 import com.github.probelog.Movement;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class PanelBuilder {
 
-    private String file, fromState, toState;
+    private Map<String, FromTo> fileStates = new HashMap<>();
+
     public void introduce(String file, String state) {
-        this.file=file;
-        fromState=state;
+        fileStates.put(file,new FromTo(state));
     }
 
     public void position(String file, String state) {
-        toState=state;
+        fileStates.get(file).to=state;
     }
 
-    public Movement getPanel() {
-        return new Movement(file, fromState, toState);
+    public Set<Movement> getPanel() {
+
+        Set<Movement> result = new HashSet<>();
+        for (String file : fileStates.keySet()) {
+            FromTo fromTo = fileStates.get(file);
+            result.add(new Movement(file, fromTo.from, fromTo.to));
+        }
+        return result;
+
+    }
+
+    private static class FromTo{
+
+        String from, to;
+
+        public FromTo(String from) {
+            this.from = from;
+        }
+
     }
 }
--- src/test/java/com/github/probelog/PanelBuilderTest.java
+++ src/test/java/com/github/probelog/PanelBuilderTest.java
@@ -5,6 +5,10 @@ import com.github.probelog.Movement;
 import com.github.probelog.PanelBuilder;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
+import static java.util.Arrays.*;
 import static org.junit.Assert.assertEquals;
 
 public class PanelBuilderTest {
@@ -15,7 +19,10 @@ public class PanelBuilderTest {
         PanelBuilder builder = new PanelBuilder();
         builder.introduce("file1","state1");
         builder.position("file1", "state2");
-        assertEquals(new Movement("file1","state1","state2"), builder.getPanel());
+        builder.introduce("file2","state3");
+        builder.position("file2", "state4");
+        assertEquals(new HashSet<Movement>(asList(new Movement("file1","state1","state2"),
+                new Movement("file2","state3","state4"))),builder.getPanel());
 
     }
 
