ae89e36e39e509d4f0e0962afbacbb53284b31c2
dave.halpin <dave.halpin@synchronoss.com>
2020-04-30 23:56:38 +0100

    No longer need to save file name in FileEvent

--- src/main/java/com/github/probelog/Linker.java
+++ src/main/java/com/github/probelog/Linker.java
@@ -1,14 +1,20 @@
 package com.github.probelog;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Linker {
 
     private FileEvent head;
+    private Map<String, FileEvent> fileEventsMap = new HashMap<>();
 
     public FileEvent addFileUpdate(String file) {
 
-        FileEvent previousEventForFile = head==null ? null : head.findEventForFile(file);
+        FileEvent previousEventForFile = fileEventsMap.containsKey(file) ? fileEventsMap.get(file) : createInitialState(file);
         head = new FileEvent(file, head, previousEventForFile!=null ? previousEventForFile : createInitialState(file));
+        fileEventsMap.put(file, head);
         return head;
+
     }
 
     private static FileEvent createInitialState(String file) {
