9cf00c56f77e4ac6d6b8c1179ba7e1ad3ca4f933
dave.halpin <dave.halpin@synchronoss.com>
2020-06-06 19:30:07 +0100

    A change is as good as a rest PS

--- src/test/java/com/github/probelog/MakingChanges.java
+++ src/test/java/com/github/probelog/MakingChanges.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class MakingChanges {
 
     @Test
-    public void initialisation() {
+    public void sequentialChange() {
 
         ChangeMaker maker = new ChangeMaker();
         Change first = maker.makeChange();
