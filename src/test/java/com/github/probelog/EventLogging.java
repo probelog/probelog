9f4d4be1da34a233c0b184affc41d321bbe97b19
dave.halpin <dave.halpin@synchronoss.com>
2020-06-09 19:09:57 +0100

    Introducing Event tracking

--- src/test/java/com/github/probelog/EventLogging.java
+++ src/test/java/com/github/probelog/EventLogging.java
@@ -15,6 +15,8 @@ public class EventLogging {
         assertEquals(UNKNOWN, logger.state("x"));
         logger.logCreate("x");
         assertEquals(CREATED, logger.state("x"));
+        // TODO
+        //assertEquals(new FileState.createCreateState("x"), logger.head().state());
 
         assertEquals(UNKNOWN, logger.state("y"));
         logger.logInitialize("y","yValue");
