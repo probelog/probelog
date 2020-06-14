282abad4f55e77c2719d65cb9a491d4284ab4eb1
dave.halpin <dave.halpin@synchronoss.com>
2020-06-14 22:37:11 +0100

    Plan for Change

---
+++ src/test/java/com/github/probelog/ChangeTest.java
@@ -0,0 +1,25 @@
+package com.github.probelog;
+
+import org.junit.Test;
+
+public class ChangeTest {
+
+    /*
+
+    Decided to to do this before test run logging as these tests
+    should ask more fundamental questions
+    and nurture the key design elements
+
+    1) Simple Change (i.e. two nearest events for same file.)
+       - Update Change
+       - Create Change
+       - Delete Change
+       - No Change (e.g. two deletes, or 2 updates (with same file value))
+
+    2) Episode Change (have to go back beyond a specific start event of "episode")
+
+
+    3) Aggregate Episode Change - get all the changes fro an episode.
+     */
+
+}
