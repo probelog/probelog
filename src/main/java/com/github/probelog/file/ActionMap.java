1dfa8544a930f26f4b582bb31c6419703890ce58
dave.halpin <dave.halpin@synchronoss.com>
2020-06-29 13:38:47 +0100

    Tidying

--- src/main/java/com/github/probelog/file/ActionMap.java
+++ src/main/java/com/github/probelog/file/ActionMap.java
@@ -3,22 +3,23 @@ package com.github.probelog.file;
 import java.util.HashSet;
 import java.util.Set;
 
+import static com.github.probelog.file.Action.*;
 import static java.util.Arrays.asList;
 
 public class ActionMap {
 
-    private static Set<Action> existingAndValid = createActionSet(Action.CREATED, Action.INITIALIZED, Action.UPDATED, Action.PASTED, Action.COPIED);
-    private static Set<Action> deleted = createActionSet(Action.DELETED, Action.CUT);
+    private static Set<Action> existingAndValid = createActionSet(CREATED, INITIALIZED, UPDATED, PASTED, COPIED);
+    private static Set<Action> deleted = createActionSet(DELETED, CUT);
 
-    static Set<Action> validFollowOnActions(Action previousAction) {
-        if (previousAction== Action.UNKNOWN)
-            return createActionSet(Action.CREATED, Action.INITIALIZED, Action.NOT_EXISTING);
-        if (previousAction== Action.NOT_EXISTING)
-           return createActionSet(Action.PASTED);
-        if (existingAndValid.contains(previousAction))
-            return createActionSet(Action.DELETED, Action.CUT, Action.COPIED, Action.PASTED, Action.UPDATED);
-        if (deleted.contains(previousAction))
-            return createActionSet(Action.CREATED, Action.PASTED, Action.NOT_EXISTING);
+    static Set<Action> validFollowOnActions(Action action) {
+        if (action== UNKNOWN)
+            return createActionSet(CREATED, INITIALIZED, NOT_EXISTING);
+        if (action== NOT_EXISTING)
+           return createActionSet(PASTED);
+        if (existingAndValid.contains(action))
+            return createActionSet(DELETED, CUT, COPIED, PASTED, UPDATED);
+        if (deleted.contains(action))
+            return createActionSet(CREATED, PASTED, NOT_EXISTING);
         throw new RuntimeException("BUG !! Broken State Map !!");
     }
 
