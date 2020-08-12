c71dcf357818ea885a8084a4d1d5558a86aae728
dave.halpin <dave.halpin@synchronoss.com>
2020-08-12 11:50:32 +0100

    Run-Stagger plan

---
+++ src/test/java/com/github/probelog/episode/RunningStaggering.java
@@ -0,0 +1,21 @@
+package com.github.probelog.episode;
+
+import org.junit.Test;
+
+public class RunningStaggering {
+
+    /*
+
+    Stagger (Test First Programming) - Two or more reds where first red contains a test class change
+            (Refactoring) - One or more reds where first red does not contain a test class change
+
+    Will simplify above so dont have to go to bother of checking if test run contains a test class change or not and just have a
+    stagger as 2 or more reds
+
+    Stagger ends on a green (so the green is inclusive), unless no greens at end (Abandoned Stagger)
+
+    Run - anything that is not in a stagger
+
+     */
+
+}
