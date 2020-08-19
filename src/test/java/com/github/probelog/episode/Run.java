bbd38822994caa9497220101ecf682e3e74175c7
dave.halpin <dave.halpin@synchronoss.com>
2020-08-19 22:53:50 +0100

    Planning Run Finding

--- src/test/java/com/github/probelog/episode/Run.java
+++ src/test/java/com/github/probelog/episode/Run.java
@@ -2,4 +2,16 @@ package com.github.probelog.episode;
 
 public class Run {
 
+    /*
+
+    Run - Green tests end with two reds
+        - One Green one Red returns a FailPass Episode with the 2 children (the fail and the pass)
+        - A Run starts with A FailPass and continues with greens
+        - A Run ends with a FailPass and starts with greens
+        - A Run ends with a single red (i.e. last test is red)
+        - A Run ends with the last green being also the last test
+        - A Run ends with A FailPass with the green in the FailPass being the last test
+
+     */
+
 }
