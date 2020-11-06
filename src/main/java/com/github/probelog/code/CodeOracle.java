8ecb6198ad537e23909fdfcfc9c7e8d7b08dc986
dave.halpin <dave.halpin@synchronoss.com>
2020-11-06 19:57:48 +0000

    CodeOracle

---
+++ src/main/java/com/github/probelog/code/CodeOracle.java
@@ -0,0 +1,17 @@
+package com.github.probelog.code;
+
+import com.github.probelog.file.FileChange;
+
+import java.util.List;
+
+public class CodeOracle {
+
+    public boolean isRefactoring(List<FileChange> fileChanges) {
+        return false;
+    }
+
+    public List<String> changeDescription(List<FileChange> fileChanges) {
+        return null;
+    }
+
+}
