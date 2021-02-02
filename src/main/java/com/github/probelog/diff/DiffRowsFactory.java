6ae950084bacfc857fd2ff2de8a523f20b28ad94
dave.halpin <dave.halpin@synchronoss.com>
2021-02-02 16:20:50 +0000

    DiffRow to LineChange factory 2

---
+++ src/main/java/com/github/probelog/diff/DiffRowsFactory.java
@@ -0,0 +1,31 @@
+package com.github.probelog.diff;
+
+import com.github.difflib.algorithm.DiffException;
+import com.github.difflib.text.DiffRow;
+import com.github.difflib.text.DiffRowGenerator;
+
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+public class DiffRowsFactory {
+
+    DiffRowGenerator diffRowGenerator = DiffRowGenerator.create()
+            .showInlineDiffs(true)
+            .mergeOriginalRevised(true)
+            .inlineDiffByWord(true)
+            .oldTag(f -> "--")
+            .newTag(f -> "++")
+            .build();
+
+    List<DiffRow> generateDiffRows(List<String> before, List<String> after)  {
+        try {
+            return diffRowGenerator.generateDiffRows(before, after);
+        }
+        catch(DiffException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
